1b67ad91c0cdfe3fd65472115c1b86047217eaed
sbooker <sbooker@mail.ru>
2020-02-10 09:38:42 +0300

    Initial

---
+++ src/Status.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sbooker\Workflow;
+
+use LitGroup\Enumerable\Enumerable;
+
+abstract class Status extends Enumerable
+{
+
+}
---
+++ src/Workflow.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sbooker\Workflow;
+
+use Ds\Map;
+use Ds\Set;
+
+abstract class Workflow
+{
+    protected Status $status;
+
+    protected \DateTimeImmutable $changedAt;
+
+    /**
+     * @var Map<Status, Set<Status>>
+     */
+    private ?Map $transitionMap = null;
+
+    public function __construct(Status $initialStatus)
+    {
+        $this->setStatus($initialStatus);
+    }
+
+    abstract protected function buildTransitionMap(): Map;
+
+    abstract protected function getStatusClass(): string;
+
+    final public function getChangedAt(): \DateTimeImmutable
+    {
+        return $this->changedAt;
+    }
+
+    final public function getStatus(): Status
+    {
+        return $this->status;
+    }
+
+    final public function isInStatus(Status $status): bool
+    {
+        return $this->getStatus()->equals($status);
+    }
+
+    /**
+     * @throws FlowError
+     */
+    final public function transitTo(Status $status): void
+    {
+        $statusType = $this->getStatusClass();
+        if (!($status instanceof $statusType)) {
+            throw new \RuntimeException("Invalid status type '{$status}' for " . get_class($this));
+        }
+
+        if (!$this->canTransitTo($status)) {
+            throw new FlowError("Can not transit status from {$this->getStatus()->getRawValue()} to {$status->getRawValue()}");
+        }
+        $this->setStatus($status);
+    }
+
+    final protected function setStatus(Status $status)
+    {
+        $this->status = $status;
+        $this->registerChange();
+    }
+
+    private function registerChange(): void
+    {
+        $this->changedAt = new \DateTimeImmutable();
+    }
+
+    public function canTransitTo(Status $status): bool
+    {
+        try {
+            /** @var Set<Status> $transitionStatusSet */
+            $transitionStatusSet = $this->getTransitionMap()->get($this->getStatus());
+            return $transitionStatusSet->contains($status);
+        } catch (\OutOfBoundsException $e) {
+            return false;
+        }
+    }
+
+    private function getTransitionMap(): Map
+    {
+        if (!$this->transitionMap) {
+            $this->transitionMap = $this->buildTransitionMap();
+        }
+        return $this->transitionMap;
+    }
+}
